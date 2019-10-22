d211a1182151bba88332f4da847accf19a1cbcc5
Safarali Ramazonov <mr.safarali@gmail.com>
2019-10-22 15:49:26 -0600

    Create component jsx and scss files

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,27 @@
 import React from 'react';
+import Sidebar from './Sidebar';
+import Header from './Header';
+import Features from './Features';
+import Realtors from './Realtors';
+import StoryPictures from './StoryPictures';
+import StoryContent from './StoryContent';
+import Homes from './Homes';
+import Gallery from './Gallery';
+import Footer from './Footer';
 
 const App = () => {
-    return <h1>I am test element</h1>;
+    return (
+        <>
+            <Sidebar />
+            <Header />
+            <Realtors />
+            <Features />
+            <StoryPictures />
+            <StoryContent />
+            <Homes />
+            <Gallery />
+            <Footer />
+        </>
+    );
 };
 export default App;
---
+++ src/components/StoryContent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const StoryContent = () => {
+    return <div className='story__content'>Story content</div>;
+};
+
+export default StoryContent;
