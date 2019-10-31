983730e02b8e748d40d1252e29ca33f708d47376
Safarali Ramazonov <mr.safarali@gmail.com>
2019-10-31 16:41:40 -0600

    Finish Story section

--- src/components/StoryPictures.js
+++ src/components/StoryPictures.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import story1 from '../images/story-1.jpeg';
+import story2 from '../images/story-2.jpeg';
 
 const StoryPictures = () => {
-    return <div className='story__pictures'>Story pictures</div>;
+    return (
+        <div className='story__pictures'>
+            <img
+                src={story1}
+                alt='Couple with new house'
+                className='story__img--1'
+            />
+            <img src={story2} alt='New house' className='story__img--2' />
+        </div>
+    );
 };
 
 export default StoryPictures;
