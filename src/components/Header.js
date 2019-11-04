573982189c386033eab1a6dc50bc845f298ad9ec
Safarali Ramazonov <mr.safarali@gmail.com>
2019-11-04 14:49:24 -0700

    Write Header markup and style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import logo from '../images/logo.png';
+import logoBI from '../images/logo-bi.png';
+import logoBBC from '../images/logo-bbc.png';
+import logoForbes from '../images/logo-forbes.png';
+import logoTechcrunch from '../images/logo-techcrunch.png';
 
 const Header = () => {
-    return <header className='header'>Header</header>;
+    return (
+        <header className='header'>
+            <img src={logo} alt='Nexter logo' className='header__logo' />
+            <h3 className='heading-3'>Your Own Home</h3>
+            <h1 className='heading-1'>The ultimate personal freedom</h1>
+            <button className='header__btn btn'>View our properties</button>
+            <div className='header__seenon-text'>Seen on</div>
+            <div className='header__seenon-logos'>
+                <img src={logoBBC} alt='BBC logo' />
+                <img src={logoForbes} alt='Forbes logo' />
+                <img src={logoTechcrunch} alt='Techcrunch logo' />
+                <img src={logoBI} alt='Business Insider logo' />
+            </div>
+        </header>
+    );
 };
 
 export default Header;
